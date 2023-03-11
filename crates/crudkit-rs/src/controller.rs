68fe53b0031f6aed40fe38f06dcd25710266e5c3
Lukas Potthast <privat@lukas-potthast.de>
2023-03-11 14:58:13 +0100

    Remove axum-websockets dep

--- crates/crudkit-rs/src/controller.rs
+++ crates/crudkit-rs/src/controller.rs
@@ -1,17 +1,22 @@
-use axum_websockets::WebsocketController;
 use std::sync::Arc;
+
+pub trait CkWebsocketController {
+    type Json;
+
+    fn broadcast_json(Self::Json);
+}
 
 // TODO: Get rid of this? Use aggregate specific data all the way. See context.rs and the repository field as an example.
 pub struct CrudController {
-    ws: Arc<WebsocketController>,
+    ws: Arc<dyn CkWebsocketController>,
 }
 
 impl CrudController {
-    pub fn new(ws: Arc<WebsocketController>) -> Self {
+    pub fn new(ws: Arc<dyn CkWebsocketController>) -> Self {
         Self { ws }
     }
 
-    pub fn get_websocket_controller(&self) -> &WebsocketController {
+    pub fn get_websocket_controller(&self) -> &dyn CkWebsocketController {
         self.ws.as_ref()
     }
 }
