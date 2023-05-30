66c6cbc6f18085e57bb8c3b4f6817f91b78ae9e4
Lukas Potthast <privat@lukas-potthast.de>
2023-05-30 23:02:22 +0200

    Format code

--- crudkit-web/crates/crudkit-web/src/custom_field.rs
+++ crudkit-web/crates/crudkit-web/src/custom_field.rs
@@ -20,6 +20,7 @@ impl<T: CrudDataTrait, O> PartialEq for CustomField<T, O> {
         false
     }
 }
+
 impl<T: CrudDataTrait, O> CustomField<T, O> {
     pub fn render(&self, entity: &T, field_mode: FieldMode) -> O {
         (self.renderer)(entity, field_mode)
@@ -27,14 +28,17 @@ impl<T: CrudDataTrait, O> CustomField<T, O> {
 }
 
 pub type CustomFields<T, O> = HashMap<<T as CrudDataTrait>::Field, CustomField<T, O>>;
+
 pub type CustomCreateFields<T, O> = HashMap<
     <<T as CrudMainTrait>::CreateModel as CrudDataTrait>::Field,
     CustomField<<T as CrudMainTrait>::CreateModel, O>,
 >;
+
 pub type CustomUpdateFields<T, O> = HashMap<
     <<T as CrudMainTrait>::UpdateModel as CrudDataTrait>::Field,
     CustomField<<T as CrudMainTrait>::UpdateModel, O>,
 >;
+
 pub type CustomReadFields<T, O> = HashMap<
     <<T as CrudMainTrait>::ReadModel as CrudDataTrait>::Field,
     CustomField<<T as CrudMainTrait>::ReadModel, O>,
