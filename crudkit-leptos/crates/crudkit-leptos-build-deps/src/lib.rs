066b1eb66cd1f9147c83b88d9b5853a123749dfb
Lukas Potthast <privat@lukas-potthast.de>
2023-08-28 15:45:04 +0200

    Use leptos-tiptap in crudkit-leptos-build

--- crudkit-leptos/crates/crudkit-leptos-build-deps/src/lib.rs
+++
@@ -1,2 +0,0 @@
-pub const TIPTAP_BUNDLE_MIN_JS: &str = yew_tiptap_build_deps::TIPTAP_BUNDLE_MIN_JS;
-pub const TIPTAP_JS: &str = yew_tiptap_build_deps::TIPTAP_JS;
---
+++ crudkit-leptos/crates/crudkit-leptos-build/src/lib.rs
@@ -0,0 +1,2 @@
+pub const TIPTAP_BUNDLE_MIN_JS: &str = leptos_tiptap_build::TIPTAP_BUNDLE_MIN_JS;
+pub const TIPTAP_JS: &str = leptos_tiptap_build::TIPTAP_JS;
